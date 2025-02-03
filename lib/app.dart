26767cc399e17546d7bf0770095b218f3bd541cf
John Lemuel G. Hapa <jl.g.hapa@gmail.com>
2025-02-03 23:33:55 +0800

    Splash Screen initial commit

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:irovolt_app/helpers/app_config.dart';
+import 'package:irovolt_app/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: AppConfig.name,
+      theme: ThemeData(
+        primarySwatch: AppConfig.primaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: "Irovolt"),
+    );
+  }
+}
---
+++ lib/helpers/app_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  static String name = "Irovolt";
+  static String logo = "assets/images/logo.png";
+  static MaterialColor primaryColor = Colors.grey;
+}
